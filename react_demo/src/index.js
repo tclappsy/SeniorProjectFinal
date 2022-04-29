923cc6eacde1b9124678a68bab6566ba84567ad9
tclappsy <76964260+tclappsy@users.noreply.github.com>
2022-04-28 22:18:39 -0400

    Delete react_demo directory

--- react_demo/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./components/App"; //imports all code from App file
-import './style/style.css';
-
-//renders our App file by div id root in index.html
-ReactDOM.render(App(), document.getElementById("root"));
-
